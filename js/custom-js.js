2339e420e235854d79e7d3bd6e547010fdc79565
Shohanur Rahman <48328695+Sr-Shohan@users.noreply.github.com>
2020-07-05 19:26:06 +0600

    Add files via upload

---
+++ js/custom-js.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('.slider').slick({
+		dots: true,
+		infinite: true,
+		cssEase: 'linear',
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		mobileFirst: true
+	});
+});
+
+
+
